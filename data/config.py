e3e9f83e76ce4cc98524ab5f94d41c06474d2610
Jumamuradov Anvarbek <anvarbekcoder@gmail.com>
2023-04-02 18:10:48 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -1,3 +1,4 @@
+import os
 from environs import Env
 
 # environs kutubxonasidan foydalanish
@@ -8,7 +9,7 @@ env.read_env()
 BOT_TOKEN = env.str("BOT_TOKEN")  # Bot Token
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 
-
+PROXY_URL = os.environ.get("PROXY_URL")
 DB_USER = env.str("DB_USER")
 DB_PASS = env.str("DB_PASS")
 DB_NAME = env.str("DB_NAME")
